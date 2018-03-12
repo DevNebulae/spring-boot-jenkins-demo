fae24d725c24d6f19801a32d7452dc86295ec5a5
DevNebulae <gamerkeeperson+github@gmail.com>
2018-03-12 15:41:11 +0100

    Add greeting endpoint and raise version to 0.0.2-SNAPSHOT

--- src/main/kotlin/eu/rekeningrijden/demo/jenkinsdemo/JenkinsDemoApplication.kt
+++ src/main/kotlin/eu/rekeningrijden/demo/jenkinsdemo/JenkinsDemoApplication.kt
@@ -2,6 +2,18 @@ package eu.rekeningrijden.demo.jenkinsdemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/greeting")
+class GreetingController {
+    @GetMapping
+    fun greet(): String {
+        return "Hello world!"
+    }
+}
 
 @SpringBootApplication
 class JenkinsDemoApplication
